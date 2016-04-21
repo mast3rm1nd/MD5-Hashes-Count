621ed9858780f0131fc27e7f5581bad536c4231a
mastermind <cyberdude@mail.ru>
2016-04-21 16:52:25 +0300

    +

---
+++ MD5 Hashes Count/HashWorksHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace MD5_Hashes_Count
+{
+    class HashWorksHelper
+    {
+
+
+        public static string CalculateMD5Hash(string input)
+        {
+            // step 1, calculate MD5 hash from input
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            // step 2, convert byte array to hex string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString().ToLower();
+        }
+    }
+}
---
+++ MD5 Hashes Count/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MD5_Hashes_Count
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Go_Click(object sender, RoutedEventArgs e)
+        {
+            int times = 1;
+            string targetPassword = TextBox_Password.Text;
+            string targetHash = TextBox_Hash.Text.ToLower();
+
+
+            string calculatedHash = HashWorksHelper.CalculateMD5Hash(targetPassword);
+
+            while(calculatedHash != targetHash)
+            {
+                  calculatedHash = HashWorksHelper.CalculateMD5Hash(calculatedHash);
+                  times++;
+            }
+
+            TextBox_Times.Text = times.ToString();
+            
+        }
+    }
+}
